bbf6583e3a711ba33a729297eb1f9931d7640b28
zhuzhengjian <zhuzhengjian@powerpms.com>
2025-08-18 16:38:27 +0800

    ci: change build

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,5 +1,15 @@
 import { defineConfig } from 'tsdown'
 
 export default defineConfig({
-
+  entry: {
+    index: './lib/index.ts',
+    vite: './lib/vite.ts',
+    prod: './lib/prod.ts',
+  },
+  dts: true,
+  clean: true,
+  outDir: './dist',
+  format: 'esm',
+  tsconfig: './tsconfig.lib.json',
+  external: ['vite'],
 })
